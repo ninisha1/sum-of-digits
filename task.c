70401adf601d5d983dffe4905a3402875225af8a
ninisha1 <86066747+ninisha1@users.noreply.github.com>
2021-08-24 20:47:01 +0530

    Create task.c

---
+++ task.c
@@ -0,0 +1,26 @@
+using namespace std; 
+ 
+int sum_of_digit(int n) 
+{ 
+
+    if (n == 0) 
+
+    return 0; 
+
+    return (n % 10 + sum_of_digit(n / 10)); 
+} 
+ 
+
+int main() 
+{ 
+
+    int num = 1234; 
+
+    int result = sum_of_digit(num); 
+
+    cout << "Sum of digits in "<< num 
+
+       <<" is "<<result << endl; 
+
+    return 0;
+}
